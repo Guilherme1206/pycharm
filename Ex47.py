b0b8356f032a9f798d6f53be3c9a86c0c070fd96
Guilherme1206 <63076035+Guilherme1206@users.noreply.github.com>
2020-04-05 21:07:39 -0300

    Update Ex47.py

--- Ex47.py
+++ Ex47.py
@@ -3,6 +3,8 @@ pares que estão no intervalo entre 1 e 50."""
 print("Os números pares entre 1 e 50 são:")
 
 for c in range(1, 51, 2):
-    print(c + 1, end=", ")
-
+    if c<=48:
+        print(c + 1, end=", ")
+    else:
+        print(c + 1, end="")
 input()
